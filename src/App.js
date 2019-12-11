4f8da76739a3b065ce1a2904c4fb83bb16b62b69
viscountfam <V3ctor42!>
2019-12-10 22:18:24 -0500

    I've got the calculator looking something like it should

--- src/App.js
+++ src/App.js
@@ -25,12 +25,11 @@ function App() {
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */} 
           <Display hello={0} />
-          <div classname="holder">
-          <SpecialButton />
-          <NumberButton />
+          <div className="holder">
+            <SpecialButton />
+            <NumberButton />
+            <OperatorButton />
           </div>
-          <OperatorButton />
-          
       </div>
     </div>
   );
